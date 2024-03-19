414ef30890bdf81b04dd12c0b95c21acc1dca68d
yasef <mustafasahin5529@gmail.com>
2024-03-19 15:56:58 +0300

    NoiseParamWord

---
+++ DCC/KbysEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCC
+{
+    public class KbysEntity
+    {
+        public string CustomerName { get; set; }
+        public string OrderNo { get; set; }
+        public string Device { get; set; }
+        public string Manufactuer { get; set; }
+        public string Type { get; set; }
+        public string SerialNumber { get; set; }
+        public string DateOfCalibration { get; set; }
+    }
+}
